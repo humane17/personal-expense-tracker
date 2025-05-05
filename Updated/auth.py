154431432e2801316176376a0c8b2454ba9f565f
humane17 <subramaniamaparna123@gmail.com>
2025-05-05 21:46:09 +0530

    shifted and reconfigured secret

--- Updated/auth.py
+++ Updated/auth.py
@@ -3,10 +3,11 @@ from fastapi.security import OAuth2PasswordBearer
 import jwt
 from jwt.exceptions import InvalidTokenError
 from datetime import datetime, timedelta, timezone
+from . import secret
 
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
+SECRET_KEY = secret.key
+ALGORITHM = secret.algo
 ACCESS_TOKEN_EXPIRE_MINUTES = 4
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
+from Updated import secret
 
-engine = create_engine("postgresql://postgres:postgres@localhost/postgres", echo=True)
+engine = create_engine(secret.database_connection, echo=True)
 
 Session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
